4ba61d310ac9da92254bdc9552881ba212636e5d
shivasanthosh17071 <shivasanthosh1707@gmail.com>
2025-05-12 17:37:51 +0530

    Update customer.js

--- view/customer.js
+++ view/customer.js
@@ -7,28 +7,28 @@ const jwt = require('jsonwebtoken')
 CustomerRouter.post('/signup', signup )
 CustomerRouter.post( '/login' , login )
 
-CustomerRouter.get('/getUsers', (req,res,next)=>{
-   let token = (req.headers.authorization)
-   if(!token){
-    res.json({Message :" Token is required"})
-   }
+// CustomerRouter.get('/getUsers', (req,res,next)=>{
+//    let token = (req.headers.authorization)
+//    if(!token){
+//     res.json({Message :" Token is required"})
+//    }
   
-   try{ let result = jwt.verify( token, "shh")
-    console.log(result)
-    if(result.Role == 1 ){
-    next(); 
-    } else{
-    return res.json({
-        message : " You are not a Admin ,only admins can access"
-    })
-    }
+//    try{ let result = jwt.verify( token, "shh")
+//     console.log(result)
+//     if(result.Role == 1 ){
+//     next(); 
+//     } else{
+//     return res.json({
+//         message : " You are not a Admin ,only admins can access"
+//     })
+//     }
    
-     } catch(err){
-    res.json(err);
-   }
+//      } catch(err){
+//     res.json(err);
+//    }
  
-    return console.log("testing")
-} , getUsers )
-
+//     return console.log("testing")
+// } , getUsers )
+CustomerRouter.get("/getUsers", getUsers);
 
 module.exports= CustomerRouter
